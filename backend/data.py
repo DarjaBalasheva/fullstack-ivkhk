a4430f2668dfbf00e04fcf5cbb9c00541a8b3c56
DarjaBalasheva <balasheva.98@mail.ru>
2023-03-18 14:52:18 +0200

    fix

--- backend/data.py
+++ backend/data.py
@@ -6,29 +6,39 @@ students_dict = [
         "project": "Anna's project",
         "typeproject": "Animatsioon",
         "uuid": "1",
+        "year": "2022"
     },  # элемент массива
     {
         "name": "Roman",
         "group": "NKMMp21",
         "project": "Roman's project",
         "typeproject": "Helindamine",
+        "uuid": "2",
+        "year": "2022"
+
     },
     {
         "name": "Ivan",
         "group": "NKMM22",
         "project": "Ivans's project",
         "typeproject": "Graafika",
+        "uuid": "3",
+        "year": "2023"
     },
     {
         "name": "Marika",
         "group": "NKMMp22",
         "project": "Marika's project",
         "typeproject": "3D Graafika",
+        "uuid": "4",
+        "year": "2023"
     },
     {
         "name": "Anna",
         "group": "NKMMp22",
         "project": "Anna's project 2",
         "typeproject": "Foto tööde",
+        "uuid": "5",
+        "year": "2023"
     },
 ]
