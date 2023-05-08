393f232f8a4ecf0407acdee71022350f1a64a90f
DarjaBalasheva <102620846+DarjaBalasheva@users.noreply.github.com>
2023-05-08 16:37:28 +0300

    Find_item (#13)
    SQL command for group

--- backend/main.py
+++ backend/main.py
@@ -8,13 +8,14 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from functions import *
+from db_connect import connect
+import base64
 
 
 
 app = FastAPI()
+app.db = connect()
 
-
-project_img = "../static/img/project_mg/project2.jpeg"
 app.mount("/static", StaticFiles(directory="../static"), name="static")
 templates = Jinja2Templates(directory="../templates")
 
@@ -28,15 +29,26 @@ def home(request: Request):
 
 @app.get("/result", response_class=HTMLResponse)
 async def read_items(request: Request, message: Union[str, None] = Query(default=None), key: Union[str, None] = Query(default=None)):
+    conn = request.app.db
+    cursor = conn.cursor()
     if message:
         if key:
-            if key == 'kõik':
+            if key == 'all':
+                # cursor.execute("SELECT * FROM medias")
                 result = find_everyone(students_dict, message)
             elif key == 'top':
                 result = find_better(students_dict, message)
             else:
                 try:
-                    result = find_item(students_dict, key, message)
+                    sql = f'''SELECT  project_info.project_uuid, project_info.project_name, project_info.year_, 
+                    type_list.type_, group_list.group_, students.first_name, students.last_name FROM
+                           project_info 
+JOIN type_list ON type_list.type_uuid = project_info.type_uuid
+JOIN group_list ON group_list.group_uuid = project_info.group_uuid
+JOIN students ON students.project_uuid = project_info.project_uuid
+                    WHERE {key} = %s'''
+                    cursor.execute(sql, (message,))
+                    result = dict_create(cursor.fetchall())
                 except KeyError:
                     return templates.TemplateResponse("notFound.html",
                         {
@@ -48,7 +60,16 @@ async def read_items(request: Request, message: Union[str, None] = Query(default
             if key == 'top':
                 result = find_all_better(students_dict)
             else:
-                result = find_all(students_dict)["data"]
+                sql = f'''SELECT  project_info.project_uuid, project_info.project_name, project_info.year_, 
+                                    type_list.type_, group_list.group_, students.first_name, students.last_name FROM
+                                           project_info 
+                JOIN type_list ON type_list.type_uuid = project_info.type_uuid
+                JOIN group_list ON group_list.group_uuid = project_info.group_uuid
+                JOIN students ON students.project_uuid = project_info.project_uuid'''
+
+                cursor.execute(sql)
+                result = dict_create(cursor.fetchall())
+
     
     if not result:
         return templates.TemplateResponse("notResult.html", {
@@ -57,6 +78,7 @@ async def read_items(request: Request, message: Union[str, None] = Query(default
             "title": "Not Result",
         },
     )
+
     else:
         return templates.TemplateResponse(
             "result.html",
@@ -67,6 +89,7 @@ async def read_items(request: Request, message: Union[str, None] = Query(default
                 "title": "Result",
             },
         )
+
 
 
 @app.get("/result", response_class=HTMLResponse, status_code=500)
@@ -99,3 +122,16 @@ async def project_page(
                             "title": "Not Found",
                         },
                     status_code=404)
+
+@app.get("/db", response_class=HTMLResponse)
+def home(request: Request):
+    conn = request.app.db
+    cursor = conn.cursor()
+    cursor.execute("SELECT data_ FROM medias")
+    img_result: tuple = cursor.fetchone()
+    img_result: bytes = base64.b64encode(img_result[0])
+    img_result: str = img_result.decode('utf-8')
+
+    return templates.TemplateResponse("img.html", {
+                "request": request,
+                "app_file_primary": img_result})
