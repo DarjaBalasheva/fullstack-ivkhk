1ea57178ca613331a6f47b426436add7641378ab
DarjaBalasheva <balasheva.98@mail.ru>
2023-05-16 15:30:28 +0300

    media_content

--- backend/functions.py
+++ backend/functions.py
@@ -45,4 +45,10 @@ def dict_create(tuple_):
     row = dict(row)
     dict_lict.append(row)
   return dict_lict
+def create_media_list(file_dict):
+  file_list = []
+  for i in range(2,6):
+    if file_dict[f'app_{i}'] is not None:
+      file_list.append(file_dict[f'app_{i}'])
+  return file_list
 
--- backend/main.py
+++ backend/main.py
@@ -32,10 +32,9 @@ def home(request: Request):
 @app.get("/result", response_class=HTMLResponse)
 async def read_items(request: Request, message: Union[str, None] = Query(default=None), key: Union[str, None] = Query(default=None)):
     conn = request.app.db
+    cursor = conn.cursor()
     cursor = conn.cursor(dictionary=True)
-    # key_dict = {
-    #     'name' : 'first_name OR last_name'
-    # }
+
     if message and key:
         if key == 'all':
             try:
@@ -67,8 +66,8 @@ async def read_items(request: Request, message: Union[str, None] = Query(default
         elif key == 'name':
             try:
                 message
-                sql = f'''SELECT  project_info.project_uuid, project_info.project_name, project_info.year_, 
-                type_list.type_, group_list.group_, students.first_name, students.last_name
+                sql = f'''SELECT project_info.project_uuid, project_info.project_name, project_info.year_, 
+                type_list.type_, group_list.group_, students.*
 
                 FROM project_info
 
@@ -76,8 +75,9 @@ async def read_items(request: Request, message: Union[str, None] = Query(default
                 JOIN group_list ON group_list.group_uuid = project_info.group_uuid
                 JOIN students ON students.project_uuid = project_info.project_uuid
 
-                WHERE first_name LIKE %s OR last_name LIKE %s'''
-                cursor.execute(sql, (message + '%', message + '%'))
+                WHERE first_name LIKE %s OR last_name LIKE %s OR CONCAT(first_name, ' ', last_name) LIKE %s OR CONCAT(last_name, ' ', first_name) LIKE %s'''
+                cursor.execute(sql, (message + '%', message + '%', '%' + message, '%' + message + '%'))
+
                 result = dict_create(cursor.fetchall())
             except KeyError:
                 return templates.TemplateResponse("notFound.html",
@@ -99,14 +99,10 @@ async def read_items(request: Request, message: Union[str, None] = Query(default
                 
                 WHERE {key} LIKE %s '''
                 cursor.execute(sql, (message+'%',))
+
                 result = dict_create(cursor.fetchall())
                 print(result[0]['student_uuid'])
-                # uuid = result[0]['student_uuid']
-                # print(uuid)
-                # print(type(uuid))
-                # uuid: str = uuid.decode('utf-8')
-                # result[0]['student_uuid'] = uuid
-                # print(type(result[0]['student_uuid']))
+
             except KeyError:
                 return templates.TemplateResponse("notFound.html",
                     {
@@ -115,6 +111,24 @@ async def read_items(request: Request, message: Union[str, None] = Query(default
                     },
                 status_code=404)
 
+
+    # else:
+    #     try:
+    #         sql = f'''SELECT project_info.project_uuid, project_info.project_name, project_info.year_,
+    #                             type_list.type_, group_list.group_, students.* FROM
+    #                                    project_info
+    #         JOIN type_list ON type_list.type_uuid = project_info.type_uuid
+    #         JOIN group_list ON group_list.group_uuid = project_info.group_uuid
+    #         JOIN students ON students.project_uuid = project_info.project_uuid'''
+    #         cursor.execute(sql, (message + '%',))
+    #         result = dict_create(cursor.fetchall())
+    #     except KeyError:
+    #         return templates.TemplateResponse("notFound.html",
+    #                                           {
+    #                                               "request": request,
+    #                                               "title": "Not Found",
+    #                                           },
+    #                                           status_code=404)
 
     elif key == 'top' and not message:
         result = find_all_better(students_dict)
@@ -163,48 +177,41 @@ async def project_page(
     conn = request.app.db
     cursor = conn.cursor(dictionary=True)
 
-    sql = f'''SELECT  students.student_uuid, project_info.project_name, project_info.year_, 
-        type_list.type_, group_list.group_, students.first_name, students.last_name,
-        files.*
-        FROM project_info 
-            JOIN type_list ON type_list.type_uuid = project_info.type_uuid
-            JOIN group_list ON group_list.group_uuid = project_info.group_uuid
-            JOIN students ON students.project_uuid = project_info.project_uuid
-            JOIN files ON files.project_uuid = project_info.project_uuid
-WHERE students.student_uuid = %s'''
-
+    sql = f'''SELECT project_info.project_uuid, students.student_uuid, project_info.project_name, project_info.year_, 
+                    type_list.type_, group_list.group_, students.first_name, students.last_name,
+                    files.*
+                    FROM project_info 
+                        JOIN type_list ON type_list.type_uuid = project_info.type_uuid
+                        JOIN group_list ON group_list.group_uuid = project_info.group_uuid
+                        JOIN students ON students.project_uuid = project_info.project_uuid
+                        JOIN files ON files.project_uuid = project_info.project_uuid
+            WHERE students.student_uuid = %s'''
     if project and uuid:
-        cursor.execute(sql, (uuid, ))
-        result = dict_create(cursor.fetchall())
-        print
-        # result = find_project(students_dict, uuid)
-        if not result:
+        try:
+            cursor.execute(sql, (uuid,))
+            result = dict_create(cursor.fetchall())[0]
+            print(result)
+            medias = create_media_list(result)
+            print('******', medias)
+
+        except KeyError:
             return templates.TemplateResponse("notFound.html",
                         {
                             "request": request,
                             "title": "Not Found",
                         },
                     status_code=404)
-        return templates.TemplateResponse(
-            "project.html",
-            {"request": request, "result": result[0], "title": "Project page"},
-        )
-    return templates.TemplateResponse("notFound.html",
+
+    if not result:
+        return templates.TemplateResponse("notFound.html",
+
                         {
                             "request": request,
                             "title": "Not Found",
                         },
                     status_code=404)
-
-@app.get("/db", response_class=HTMLResponse)
-def home(request: Request):
-    conn = request.app.db
-    cursor = conn.cursor()
-    cursor.execute("SELECT data_ FROM medias")
-    img_result: tuple = cursor.fetchone()
-    img_result: bytes = base64.b64encode(img_result[0])
-    img_result: str = img_result.decode('utf-8')
-
-    return templates.TemplateResponse("img.html", {
-                "request": request,
-                "app_file_primary": img_result})
+    else:
+        return templates.TemplateResponse(
+            "project.html",
+            {"request": request, "result": result, "medias": medias, "title": "Project page"},
+        )
