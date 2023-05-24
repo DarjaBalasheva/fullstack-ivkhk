8c53d54514b2a01fc2d4eee8f4c17835a783f302
DarjaBalasheva <102620846+DarjaBalasheva@users.noreply.github.com>
2023-05-24 17:49:08 +0300

    Sql (#17)
    * Find_item
    
    SQL command for group
    
    * JSON_fromat
    
    * uuid varchar(36)
    
    * media_content
    
    * fix style
    
    * fix
    
    * fix bags
    
    * fix bags
    
    * icon
    
    * Scroll
    
    * Create .gitignore

--- backend/functions.py
+++ backend/functions.py
@@ -47,7 +47,7 @@ def dict_create(tuple_):
   return dict_lict
 def create_media_list(file_dict):
   file_list = []
-  for i in range(2,6):
+  for i in range(2,26):
     if file_dict[f'app_{i}'] is not None:
       file_list.append(file_dict[f'app_{i}'])
   return file_list
