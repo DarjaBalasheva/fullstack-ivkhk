981bf87c0740f298c33159a11156f8e23892a5b7
DarjaBalasheva <balasheva.98@mail.ru>
2023-08-16 11:39:56 +0300

    Data-example

--- backend/functions.py
+++ backend/functions.py
@@ -1,14 +1,14 @@
 def dict_create(tuple_):
-  dict_lict = []
-  for row in tuple_:
-    row = dict(row)
-    dict_lict.append(row)
-  return dict_lict
+    dict_lict = []
+    for row in tuple_:
+        row = dict(row)
+        dict_lict.append(row)
+    return dict_lict
+
+
 def create_media_list(file_dict):
-  file_list = []
-  for i in range(2,26):
-    if file_dict[f'app_{i}'] is not None:
-      file_list.append(file_dict[f'app_{i}'])
-  return file_list
-
-
+    file_list = []
+    for i in range(2, 26):
+        if file_dict[f"app_{i}"] is not None:
+            file_list.append(file_dict[f"app_{i}"])
+    return file_list
